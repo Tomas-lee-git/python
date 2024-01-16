b0192460726dbb9155c5c85594082575891494f1
Muli <muli9611@gmail.com>
2024-01-16 23:36:49 +0800

    feat(dict): dict data and for in loop

---
+++ dict.py
@@ -0,0 +1,27 @@
+"""
+    dict => dictionaries
+        associate one value with another literally
+        keys、values
+        访问value的方法 => dict["key"]
+
+
+"""
+
+students = {
+    "Tom": "Cat",
+    "Jimmy": "Mouse",
+    "Bob": "Cat",
+    "Down": "Dog"
+}
+
+print(students["Tom"])
+
+#⚠️ for in 循环，在dict 中拿到的是 key，在 list 中拿到的 value
+for student in students:
+    match students[student]:
+        case "Cat":
+            print(student, "meow", sep=", ")
+        case "Dog":
+            print(student, "wow", sep=", ")
+        case _:
+            print(student, "zizi", sep=", ")
