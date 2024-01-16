1a500282198b8aa16c8f55dc7dcada9537ce9e78
Muli <muli9611@gmail.com>
2024-01-16 19:18:05 +0800

    feat(print): print str with /n and end param

---
+++ print.py
@@ -0,0 +1,21 @@
+# 不推荐使用，但是知道 print 有这么个功能
+
+print("wow" * 3)
+# 连在一起了
+# wowwowwow
+
+print("wow\n" * 3)
+# 末尾多了一个空行
+"""
+    wow
+    wow
+    wow
+
+"""
+print("wow\n" * 3, end="")
+#完美解决👍
+"""
+    wow
+    wow
+    wow
+"""
