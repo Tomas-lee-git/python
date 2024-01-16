d87b82b7e5836ccb7c1a9f9008039e540a96dc19
Muli <muli9611@gmail.com>
2024-01-16 21:59:58 +0800

    feat(indexAndLen): use len() and index in list

---
+++ indexAndLen.py
@@ -0,0 +1,21 @@
+"""
+    list:
+        list[index],可以用 index 取出 list 中的一个具体的内容；
+        for in 循环中，i 代表的直接是 list 中的具体元素；
+        python 中除了 list，还有其它 iterable 类型的数据，都可以用到 for 循环；
+    len():
+        len function will return iterable data's length；
+        使用 len() 函数获取 list 的长度，
+        然后借用 range() 函数生成一个 index list，
+        最后用 i 和 list[i] 来分别获取 index 和 element；
+"""
+
+students = ["Bob", "Tom", "Eric"]
+
+print(students[0])
+
+for student in students:
+    print(student)
+
+for i in range(len(students)):
+    print(i, students[i])
--- listAndFor.py
+++ listAndFor.py
@@ -2,7 +2,7 @@
     for，可以用来循环 list 数据
     list: [1,2,3]
     range(n)函数:
-        n: 指定list的内容：0-n，不包括n;
+        n: 数字 n，指定 list 的长度为0，内容为：0-n，不包括 n;
         return: list [0,……,n-1]
     _:
         在 python 中，如果定义的参数不需要直接使用，习惯使用 _ 来表示
