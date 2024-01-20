8fa2f9bc4f09e57e24ac0405113c4aa54d470710
Muli <muli9611@gmail.com>
2024-01-20 21:41:54 +0800

    feat(sysModule):  use sys.exit function to exit program proactively

--- sysModule.py
+++ sysModule.py
@@ -1,11 +1,16 @@
 """
-    一、sys.argv:
-        用 list 的方式记录了运行 python3 时后面的内容：
-            1. sys.argv[0]: program name，也就是 *.py
-            2. sys.argv[1]: something else input
-            3. 字符串输入的，即使有空格，再长都算一个喽
-            4. 如果没有输入，或者输入的内容对应 list 的 index 不存在：
-                IndexError: list index out of range
+    一、sys:
+        sys.argv:
+            argv => argument vector: 参数向量
+            用 list 的方式记录了运行 python3 时后面的内容：
+                1. sys.argv[0]: program name，也就是 *.py
+                2. sys.argv[1]: something else input
+                3. 字符串输入的，即使有空格，再长都算一个喽
+                4. 如果没有输入，或者输入的内容对应 list 的 index 不存在：
+                    IndexError: list index out of range
+        sys.exit:
+            exit program and recept a string parameter as prompt message
+
 
     二、如果加入了不必要的 indentation(缩进)，会报错：😄
         IndentationError: unexpected indent
@@ -19,7 +24,8 @@ import sys
 
 def main():
     # sys_arg_version_1()
-    sys_arg_version_2()
+    # sys_arg_version_2()
+    sys_arg_version_3()
 
 #　版本1，处理报错
 def sys_arg_version_1():
@@ -39,5 +45,16 @@ def sys_arg_version_2():
         print("please add your name before hit Enter key")
     else:
         print("too many arguments, please just input one")
+
+# 版本3，及时退出
+def sys_arg_version_3():
+    argv_list = sys.argv
+    if len(argv_list) > 2:
+        sys.exit("too many arguments, please just input one")
+    elif len(argv_list) < 2:
+        sys.exit("please add your name before hit Enter key")
+
+    # 主要目的应该和 exception 处理区分开来
+    print(f"hello, my name is {sys.argv[1]}")
 
 main()
