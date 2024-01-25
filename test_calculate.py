1d35676b91e98be82576fb5e0eb5961e1d626b12
Tomas-lee-git <muli9611@gmail.com>
2024-01-25 22:42:54 +0800

    feat(test_calculate): too much test code to complete in real work 😅

--- test_calculate.py
+++ test_calculate.py
@@ -25,12 +25,23 @@ def test_square():
     try:
         assert square(2) == 4
     except AssertionError:
-        print("2 squared not equal 4")
+        print("2 squared wasn't 4")
     try:
         assert square(3) == 9
     except AssertionError:
-        print("3 squared not equal 9")
-
+        print("3 squared wasn't 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared wasn't 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared wasn't 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared wasn't 0")
 if __name__ == "__main__":
     main()
 
