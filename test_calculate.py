94f3c8fdb1de802333834e3d8ae7522eb981b8e4
Tomas-lee-git <muli9611@gmail.com>
2024-01-25 22:27:24 +0800

    feat(test_calculate): use assert and AssertionError to replace custom test logic

--- test_calculate.py
+++ test_calculate.py
@@ -1,8 +1,16 @@
 """
-    python test convention:
+    一、python test convention:
         1. if you want to test a module: a, create a file named as test_a.py;
         2. if you want to test a function: b, create a function named as test_b;
         3. don't testing one thing but test serval and make sure cover case;
+
+    二、assert:
+        1. the key word to sort of boldly claim that something is true;
+        2. if something is not true, will see some kind of error on the screen;
+        3. AssertionError:
+            display Traceback, detail but not friendly to read
+            use except to enhance readable
+
 
 """
 
@@ -14,11 +22,14 @@ def main():
     test_square()
 
 def test_square():
-    if square(2) != 4:
-        print(f"Test failed: 2 squared should be 4, but is {square(2)}")
-
-    if square(3) != 9:
-        print(f"Test failed: 3 squared should be 9, but is {square(3)}")
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared not equal 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 squared not equal 9")
 
 if __name__ == "__main__":
     main()
