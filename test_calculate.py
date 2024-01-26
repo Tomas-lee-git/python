f08466d57f2c240f616e0df0e1fe1d710458620c
Tomas-lee-git <muli9611@gmail.com>
2024-01-26 20:15:55 +0800

    feat(test_calculate): divide multiple functions to test, because pytest will stop when some case fails

--- test_calculate.py
+++ test_calculate.py
@@ -27,14 +27,21 @@ import sys
 from calculate import square
 
 def main():
-    test_square()
+    test_square_positive()
+    test_square_negative()
+    test_square_zero()
 
-def test_square():
+def test_square_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+def test_square_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+
+def test_square_zero():
     assert square(0) == 0
+
 
 if __name__ == "__main__":
     main()
