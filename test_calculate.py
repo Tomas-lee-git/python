ac36b7c8b63324759c7814d3439c6dd42722a13f
Tomas-lee-git <muli9611@gmail.com>
2024-01-26 22:13:09 +0800

    feat(test_calculate): use "with pytest.raises(error_type)" to test error except

--- test_calculate.py
+++ test_calculate.py
@@ -16,11 +16,15 @@
         2. handled the try、except、print all of standardization of actually running these tests;
         3. execute "pytest", the implementation will be automatically added: try, except, print, etc.;
         4. unit tests are typically tests for functions that you have written;
+        5. with pytest.raises(error_type):
+            f(n)
+           在执行f(n)时，预期某种特定类型的错误，这样可以确保所有会发生的 error 都已经被考虑到且已经用 try、catch 处理到位了
 
 
 
 
 """
+import pytest
 
 import sys
 
@@ -30,6 +34,7 @@ def main():
     test_square_positive()
     test_square_negative()
     test_square_zero()
+    test_square_input()
 
 def test_square_positive():
     assert square(2) == 4
@@ -42,6 +47,9 @@ def test_square_negative():
 def test_square_zero():
     assert square(0) == 0
 
+def test_square_input():
+    with pytest.raises(TypeError):
+        square("cat")
 
 if __name__ == "__main__":
     main()
