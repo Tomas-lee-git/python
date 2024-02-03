7a069d65dbcdefb159430150c4ed82abe811c405
Tomas-lee-git <muli9611@gmail.com>
2024-02-03 23:30:17 +0800

    feat(csv_writer): write csv file with dict by csv.DictReader(filepath,filenames)

--- csv_writer.py
+++ csv_writer.py
@@ -1,17 +1,44 @@
 """
-    csv.writer:
-        1). writer.writerow(list):
-            
+    1. csv.writer:
+        1). add row data by list [column1 content, column1 content……]:
+            writer = csv.writer(file)
+            writer.writerow(list)
+        2).add row data by dict {key1: value1;key2: value2……}:
+            csv.DictWriter(f, fieldnames) 
+                The fieldnames parameter is a sequence of keys that identify the order in which values in the dictionary passed to the writerow() method are written to file
+                    fieldnames is a required parameter because dict has no order and we need to tell the writer how to add the values in order
+            writer.writerow(dict)
+
+    2. ❕don’t mess with csv files manually😂；
+
+
 """
 
 import csv
 
+# option1: add row data by list
+
+# questions = ["name","breed","age","evaluate"]
+# answers_list = []
+
+# for question in questions:
+#     answers_list.append(input(f"what's your {question} ? "))
+
+# with open("colleagues.csv","a") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(answers_list) 
+
+
+# option2: add row data by dict {key1: value1;key2: value2……}
+
 questions = ["name","breed","age","evaluate"]
-answers = []
+answers_dict = {}
 
 for question in questions:
-    answers.append(input(f"what's your {question} ? "))
+    answers_dict[question] = input(f"what's your {question} ? ")
+
+print(f"answers_dict is {answers_dict}")
 
 with open("colleagues.csv","a") as file:
-    writer = csv.writer(file)
-    writer.writerow(answers) 
+    writer = csv.DictWriter(file, questions)
+    writer.writerow(answers_dict) 
