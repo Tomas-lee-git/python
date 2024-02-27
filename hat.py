fcd0d6e226fb215c919292e65a1ceb1f20a22e94
Tomas-lee-git <muli9611@gmail.com>
2024-02-27 23:00:18 +0800

    feat(hat): create class variables and access it in methods decorated with @classmethod and perform certain operations independent of the instance object

---
+++ hat.py
@@ -0,0 +1,26 @@
+"""
+    1. class methods:
+        1). some action to be associated with the class itself,
+            no matter what the specific objects own values or instance variables are;
+        2). keyword, @classmethod:
+            i. don't pass self any more (reference to instance object);
+            ii. pass in the reference to the class itself: cls;
+            iii. call class method by Student.sort();
+        3). class variable, can be used in any of methods by cls.variable;
+        4). instance object can call classmethod succeed;
+"""
+# import random
+from random import choice
+
+class Hat:
+    # class variable
+    house = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    @classmethod
+    def sort(cls, name):
+        print(f"{name} is in {choice(cls.house)}")
+
+hat = Hat()
+hat.sort("Harry")
+
+Hat.sort("Harry")
