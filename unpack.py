da7ecbab8659c280e477f00d32a4705a21fe70a3
Tomas-lee-git <muli9611@gmail.com>
2024-03-02 23:36:36 +0800

    feat(unpack):  naming parameters when calling a function

--- unpack.py
+++ unpack.py
@@ -10,6 +10,11 @@
         ii. for number, * means multiple;
         iii. for string, * means concatenate str with multiple numbers;
         iiii. for unpack, * means unpack list parameter to individual parameters;
+    
+    3. name of parameters:
+        i. func(a = 1, b = 2, c = 3 ) => func(a, b, c);
+        ii. if name the parameters when calling a function,
+            don't need to follow the order of parameters when the function was defined;
 
 """
 # unpack list
@@ -18,12 +23,15 @@
 # print(f"first_name is {first_name}")
 # print(f"second_name is {second_name}")
 
-# unpack
+# unpack list parameter
 
 def total(galleons, sickles, knuts):
     return(galleons * 17 + sickles) * 29 + knuts
 
-wallet = [100, 50, 25]
-
-print(f"{total(*wallet)} knuts")
+# wallet = [100, 50, 25]
+# print(f"{total(*wallet)} knuts")
 # TypeError: total() missing 2 required positional arguments: 'sickles' and 'knuts'
+
+# name of parameters
+print(f"{total(sickles = 50, galleons = 100, knuts = 25)} knuts")
+
