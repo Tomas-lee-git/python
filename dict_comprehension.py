bcf907255e3e4ac7474961cfe09aac096530224f
Tomas-lee-git <muli9611@gmail.com>
2024-03-07 10:49:18 +0800

    feat(dict_comprehension): for in loop gets only the element of the list and gets only key of the dict

--- dict_comprehension.py
+++ dict_comprehension.py
@@ -1,6 +1,6 @@
 """
     1. dict comprehension: 
-        1). d = {k:v for k, v in iterable};
+        1). d = {k:v for k in iterable};
 
     2. for in:
         1). to dict, for key in dict, get every key;
@@ -32,9 +32,18 @@ for i in range(len(students)):
     ...
 
 for index,val in enumerate(students, start = 1):
-    print(f"{index} {val}")
+    # print(f"{index} {val}")
     ...
 
 
 # for key,val in enumerate(gryffindors_dict, start = 1):
 #     print(f"{key} {val}")
+    
+# for key, val in gryffindors_dict:
+#     # print(f"{key} {val}")
+    ...
+print(f"gryffindors_dict {gryffindors_dict}")
+
+d = {k:k+"123" for k in gryffindors_dict}
+
+print(f"d is {d}")
