6139ffaa7e48371c6be0b691d6481882fa947dcf
Tomas-lee-git <muli9611@gmail.com>
2024-03-09 23:18:03 +0800

    feat(interpreter):  interactive model

---
+++ Python Tutorial/interpreter.py
@@ -0,0 +1,11 @@
+# command: python3.12, show python's information;
+
+# command + D: quit python interpreter;
+
+# sys.argv: the script name and additional arguments thereafter are turned into a list of strings and assigned to the argv variable in the sys module;
+
+# Continuation lines are needed when entering a multi-line construct(Shift + Enter);
+
+# use Enter to execute in command line
+
+# By default, Python source files are treated as encoded in UTF-8;
