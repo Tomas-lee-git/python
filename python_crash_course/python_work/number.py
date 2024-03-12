65b3a5a02e29256d5a986f436106e462050958b9
Tomas-lee-git <muli9611@gmail.com>
2024-03-12 21:45:40 +0800

    feat(number): number crunching

---
+++ python_crash_course/python_work/number.py
@@ -0,0 +1,35 @@
+# + - * / **
+n = 2 ** 3
+
+# print(n) # 8
+# print(0.1 + 0.2) # 0.30000000000000004
+# print(4 / 2) # 2.0 # all divide operate will get float number
+# print(5 / 0.5) # 10.0 # have any float number, will get float number too
+
+
+# use underscore to make number more readable, python will ignore underscore
+big_number = 100_000_000_000
+# print(big_number) # 100000000000
+
+# assign multiple variables in one line
+a, b, c = 1, "🐮", 3
+# print(f"a is {a}")
+# print(f"b is {b}")
+# print(f"c is {c}")
+
+"""
+    constant:
+        no built-in constant type data, just community agreement
+        use upper case and _
+        constant variable's value should't to be modified
+"""
+CONSTANT_NAME = "constant name"
+
+print(4+4)
+print(12-4)
+print(2*4)
+print(32/4)
+print(2**3)
+
+favorite_number = 689
+print(f"my favorite number is {favorite_number}:\n\t 6 represents success;\n\t 8 represents wealth;\n\t 9 represents health;")
