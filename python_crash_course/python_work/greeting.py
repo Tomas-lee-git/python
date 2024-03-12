13d312c9a76cbb89910c951bc9f27773a9ebb15a
Tomas-lee-git <muli9611@gmail.com>
2024-03-12 21:19:02 +0800

    feat(greeting): practice str methods

---
+++ python_crash_course/python_work/greeting.py
@@ -0,0 +1,21 @@
+name = "Lee"
+# print(f"Hello {name}, would you like to learn some Python today?")
+
+name_upper = name.upper()
+name_lower = name.lower()
+
+# print(f"name is {name}, name_upper is {name_upper}, name_lower is {name_lower}")
+
+saying = "Albert Einstein once said, 'A person who never made a mistake never tried anything new'"
+# print(saying)
+
+famous_name = "Albert Einstein"
+message = f"\t{famous_name} once said,\n\t 'A person who never made a mistake never tried anything new'  "
+# print(message)
+
+# print(message.lstrip())
+# print(message.rstrip())
+# print(message.strip())
+
+filename = "python_notes.txt"
+print(filename.removesuffix(".txt"))
