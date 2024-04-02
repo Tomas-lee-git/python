26172a968b1d8c4edf4937c3a2dbc259594bc5e5
Tomas-lee-git <muli9611@gmail.com>
2024-04-02 21:34:01 +0800

    feat(star): 从头写一个pygame，用星星铺满屏幕

---
+++ python_crash_course/python_work/projects/sky/sky.py
@@ -0,0 +1,77 @@
+"""
+    开发计划：
+        1. 渲染一颗星星；
+        2. 渲染一排星星；
+        3. 渲染多行，直到铺满屏幕的星星；
+        4. 整个屏幕随机渲染星星，达到更逼真的效果；
+"""
+import pygame
+import sys
+
+from star import Star
+
+class Sky:
+    """实现夜晚天空的类"""
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init() # 初始化游戏背景
+        self.clock = pygame.time.Clock()  # 通过self.clock 控制游戏的帧率
+        self.screen = pygame.display.set_mode((1200, 900)) # 设置游戏窗口大小
+        self.screen_rect = self.screen.get_rect() # 获取游戏窗口 rect
+        pygame.display.set_caption("Sky full of stars")
+        # # 启用游戏编组来管理众多星星✨，生成[可以被编组的实例]的类需要继承 Sprite 类
+        self.stars = pygame.sprite.Group()
+        self._create_all_stars() # 生成星空中的所有星星
+    
+    def _create_star(self, x_position, y_position):
+        """生成一个星星"""
+        star = Star(self)
+        star.rect.x = x_position
+        star.rect.y = y_position
+        self.stars.add(star)
+        
+    def _create_all_stars(self):
+        """生成众多星星"""
+        star = Star(self)
+        star_width = star.rect.width # 获取一个星星的宽度
+        star_height = star.rect.height # 获取一个星星的高度
+        current_x  = 0.3 * star_width 
+        current_y  = 0.5 * star_height 
+        # 不断地重复[生成一整行星星], 直到生成整个屏幕的星星
+        while current_y < self.screen_rect.height - star_height:
+            # 不断地生成星星，直到铺满一行
+            while current_x < (self.screen_rect.width - star_width): # 留出两个✨边距
+                self._create_star(current_x, current_y) # 不断地生成星星
+                current_x += 1.3 * star_width # 动态调整最新星星的 x 轴位置
+
+            current_y += 1.5 * star_height # 动态调整最新一行中星星的 y 轴位置
+            # 绘制完一行之后，需要重置 current_x，以便开始绘制新的一行
+            current_x = 0.3 * star_width
+        
+
+    def _check_event(self):
+        """检查事件"""
+        # 必须得添加 while 的退出条件，不然游戏就卡死了，游戏窗口都不显示了
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q: # 按 Q 退出游戏
+                    sys.exit()
+                
+        
+    def run_game(self):
+        """开始游戏的主循环"""
+        while True: # 游戏是一个不断刷新的过程，所以启动后必须不断地更新屏幕才能正常渲染游戏
+            self._check_event()
+            self.screen.fill((0, 0, 0)) # 黑色屏幕背景
+            self.stars.draw(self.screen) # 把所有星星绘制到屏幕中
+            # flip() the display to put your work on screen
+            pygame.display.flip()
+            self.clock.tick(60)  # limits FPS to 60
+        
+        
+if __name__ == "__main__":
+    sky = Sky() # 创建游戏实例
+    sky.run_game() # 启动游戏
+        
---
+++ python_crash_course/python_work/projects/sky/star.py
@@ -0,0 +1,17 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+class Star(Sprite): # 生成[可以被编组的实例]的类需要继承 Sprite 类
+    """实现满天繁星的类"""
+    def __init__(self, sky):
+        """初始化星星"""
+        super().__init__()
+        self.screen = sky.screen
+        self.screen_rect = sky.screen_rect
+        # 加载图片
+        self.image = pygame.image.load("./images/star.bmp")
+        # 获取并设置图片的 rect 属性
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width # 初始位置与(0, 0) 间隔图片本身的宽度
+        self.rect.y = self.rect.height # 初始位置与(0, 0) 间隔图片本身的高度
