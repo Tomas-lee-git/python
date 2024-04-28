a355b90f22d5c1fb0df2a0b5dc145555109fac89
Tomas-lee-git <muli9611@gmail.com>
2024-04-28 23:13:54 +0800

    feat(ship): 限制飞船只能在X方向移动

--- python_crash_course/python_work/projects/alien_invasion/ship.py
+++ python_crash_course/python_work/projects/alien_invasion/ship.py
@@ -54,10 +54,10 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > self.screen_rect.left:
             self.x -= self.settings.ship_speed
-        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
-            self.y += self.settings.ship_speed
-        if self.moving_up and self.rect.top > self.screen_rect.top:
-            self.y -= self.settings.ship_speed
+        # if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+        #     self.y += self.settings.ship_speed
+        # if self.moving_up and self.rect.top > self.screen_rect.top:
+        #     self.y -= self.settings.ship_speed
 
         # 实际更新飞船的位置
         self.rect.x = self.x
